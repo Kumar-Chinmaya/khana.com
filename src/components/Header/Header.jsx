0d1d34f5021cb7b0296e6017a2942fd756c2210e
Kumar-Chinmaya <nayakchinmaya521@gmail.com>
2024-07-31 11:18:33 +0530

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
+import { assets } from '../../assets/assets'
 
 
 const Header = () => {
   return (
     <div className='header'>
+      <img src={assets.header_img}  />
         <div className='header-contents'>
             <h2>Order your favourite food here.</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad, atque mollitia unde reiciendis ullam cupiditate corporis minus enim ut libero asperiores facere illo possimus voluptatum numquam quia, ipsa deleniti?</p>
